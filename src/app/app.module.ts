17a0ff538910e8c713a11875bf647d09a356fa9d
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-17 07:41:00 -0300

    Create table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TableComponent } from './shared/components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent // Declarado como teste
+    NavbarComponent, // Declarado como teste
+    TableComponent // Declarado como teste
   ],
   imports: [
     BrowserModule
