8a2bc2dc56cfafc1d7f0e5079521f2c150f43f88
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-18 22:50:20 -0300

    Move navbar to shared/layout folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
 import { TableComponent } from './shared/components/table/table.component';
 
 @NgModule({
