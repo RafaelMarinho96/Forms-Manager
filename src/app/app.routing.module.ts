a43c92fe339eae6e1c1ec4327523a2e3efcf9eef
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-19 21:42:35 -0300

    Added login html and styles. Adjusted login input field renamed css property ':root' to '.container'

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
     {
-        path: 'signin',
+        path: 'login',
         component: LoginComponent
     }
 ]
