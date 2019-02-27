79f86ef9b6b24336c07a9843dadbea83d3ecf2f5
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-26 23:11:29 -0300

    Created reset password routes and extract url params

--- src/app/login/login.routing.module.ts
+++ src/app/login/login.routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ResetPasswordComponent } from "./reset-password/reset-password.component";
+import { LoginComponent } from "./login.component";
 
 const LOGIN_ROUTES: Routes = [
     {
-        path: 'reset-password/:token',
-        component: ResetPasswordComponent
+        path: 'reset-password',
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'expires',
+            },
+            {
+                path: 'expires',
+                component: LoginComponent
+            },
+            {
+                path: ':email',
+                component: ResetPasswordComponent
+            }
+        ]
     }
 ]
 
--- src/app/login/reset-password/reset-password.component.ts
+++ src/app/login/reset-password/reset-password.component.ts
@@ -1,8 +1,55 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     templateUrl: './reset-password.component.html',
     styleUrls: ['./reset-password.component.scss']
 })
 
-export class ResetPasswordComponent {}
+export class ResetPasswordComponent implements OnInit{
+    
+    constructor(
+        private activateRoute: ActivatedRoute,
+        private formBuilder: FormBuilder){}
+
+    token: string;
+    email: string;
+    resetForm: FormGroup
+
+    ngOnInit(): void {
+        this.extractParams();
+        this.resetForm = this.formBuilder.group({
+            password: ['', 
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(16)
+                ]
+            ],
+            confirmPassword: ['', 
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(16)
+                ]
+            ],
+        })
+    }
+
+    extractParams(){
+        this.activateRoute.params.subscribe(
+            (params: any) => {
+                this.email = params['email'],
+                console.log(this.email)
+            }
+        )
+
+        this.activateRoute.queryParams.subscribe(
+            (params => {
+                this.token = params.token,
+                console.log(this.token)
+            })
+        )
+    }
+}
